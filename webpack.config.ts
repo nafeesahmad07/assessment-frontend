1a1bd96fa7d673a02e859cd88dbc2bbf525e28b3
nafeesahmad07 <na03344907245@gmail.com>
2025-01-25 22:16:28 +0500

    fix build issue.

---
+++ webpack.config.ts
@@ -0,0 +1,34 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader", // Add this before resolve-url-loader
+            options: {
+              postcssOptions: {
+                plugins: [require("tailwindcss"), require("autoprefixer")],
+              },
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "resolve-url-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
